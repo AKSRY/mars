2aff5203a1b4edc7b9d4ca8949f15b2bb577413f
AKSRY <58566101+AKSRY@users.noreply.github.com>
2020-04-03 19:01:20 +0900

    Add files via upload
    This func resolves a txtfile per empty line.

---
+++ file_flagm.py
@@ -0,0 +1,20 @@
+#! python3
+import os
+
+#.txtファイルを空行ごとにバラす
+def file_flagm(f_path, to_path):
+  origin = open(f_path)
+  lines = origin.readlines()
+  origin.close()
+  chunks = {}
+  while '\n' in lines:
+    for lp_0 in range(lines.index('\n')):
+      raw_chunk = ''
+      raw_chunk += lines[lp_0]
+    chunks[lines[0].strip('\n')] = raw_chunk
+    del lines[0:lines.index('\n')+1]
+  os.chdir(to_path)
+  for lp_0 in chunks.keys():
+    flagmented = open(str(lp_0)+'.txt', 'w')
+    flagmented.write(chunks[lp_0])
+    flagmented.close()
